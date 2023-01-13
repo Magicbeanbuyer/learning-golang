178d0f1f55e5fe0e6da7ee9628c42585fdf9c046
Xiatong <vanessazxt@gmail.com>
2023-01-13 14:33:38 +0100

    add slice and for loop

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// Go is statically typed
-	var card string = newCard()
-	// card := "Ace of Spades"
-	// := is used for initialization  
-	fmt.Println(card)
+	cards := []string{newCard(), "Five of Diamonds"}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards{
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
 	return "Ace of Spades" 
 }
+
+/* NOTES
+Go is statically typed
+
+:= is used for initialization 
+
+array: fixed length, elements must have identical type
+slice: varied length, elements must have identical type
+
+append does not modify the slice in place, rather return a new slice
+*/
