46929d0701ef2566103ad2bf0901b0fd1d4fce05
Xiatong <vanessazxt@gmail.com>
2023-01-13 16:22:02 +0100

    custom type and its methods

---
+++ deck.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+// d: function parameter, use type abbreviation by convention, deck: parameter type
+func (d deck) print() {
+	for i, card := range(d) {
+		fmt.Println(i, card)
+	}
+}
+
+func newDeck() deck {
+	suits := [4]string{"spades", "hearts", "diamonds", "clubs"}
+	values := [13]string{"a", "1", "2", "3", "4", "5", "6", "7", "8", "10", "j", "q", "k"}
+
+	newDeck := deck{}
+
+	for _, suit := range(suits) {
+		for _, value := range(values) {
+			newDeck = append(newDeck, value+" of "+suit)
+		}
+	}
+
+	return newDeck
+}
+
+/*
+Go does not have class, declare custom type instead
+
+string type var cannot have nil as its value
+
+when a var is declared but not initialized, so called "zero value" (0, "", false) 
+is assigned to it.
+*/
--- main.go
+++ main.go
@@ -1,20 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	cards := []string{newCard(), "Five of Diamonds"}
-	cards = append(cards, "Six of Spades")
-
-	for i, card := range cards{
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Ace of Spades" 
+	cards := newDeck()
+	cards.print()
 }
 
 /* NOTES
