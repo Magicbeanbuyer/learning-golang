fd415abfb5b36a137ea0e8125c9edab13da9163f
Xiatong Zheng <xiatong.zheng@deepl.com>
2023-01-22 21:28:23 +0100

    slice operations

--- deck.go
+++ deck.go
@@ -1,12 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type deck []string
 
 // d: function parameter, use type abbreviation by convention, deck: parameter type
 func (d deck) print() {
-	for i, card := range(d) {
+	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
@@ -17,8 +20,8 @@ func newDeck() deck {
 
 	newDeck := deck{}
 
-	for _, suit := range(suits) {
-		for _, value := range(values) {
+	for _, suit := range suits {
+		for _, value := range values {
 			newDeck = append(newDeck, value+" of "+suit)
 		}
 	}
@@ -26,11 +29,19 @@ func newDeck() deck {
 	return newDeck
 }
 
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func toString(d deck) string {
+	return strings.Join(d, ",")
+}
+
 /*
 Go does not have class, declare custom type instead
 
 string type var cannot have nil as its value
 
-when a var is declared but not initialized, so called "zero value" (0, "", false) 
+when a var is declared but not initialized, so-called "zero value" (0, "", false)
 is assigned to it.
 */
--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
+	hand, remainingDeck := deal(cards, 5)
+	//hand.print()
+	fmt.Println(toString(hand))
+	remainingDeck.print()
 }
 
 /* NOTES
 Go is statically typed
 
-:= is used for initialization 
+:= is used for initialization
 
 array: fixed length, elements must have identical type
 slice: varied length, elements must have identical type
