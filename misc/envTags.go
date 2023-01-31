8afe4fbf654f18bde74dd72849e7f2e966aa936e
Xiatong Zheng <xiatong.zheng@deepl.com>
2023-01-31 17:47:50 +0100

    feat: env tags

---
+++ misc/envTags.go
@@ -0,0 +1,28 @@
+package misc
+
+//package main
+
+import (
+	"fmt"
+	"github.com/caarlos0/env/v7"
+	"time"
+)
+
+type config struct {
+	Home         string        `env:"HOME"`
+	Port         int           `env:"PORT" envDefault:"3000"`
+	Password     string        `env:"PASSWORD,unset"`
+	IsProduction bool          `env:"PRODUCTION"`
+	Hosts        []string      `env:"HOSTS" envSeparator:":"`
+	Duration     time.Duration `env:"DURATION"`
+	TempFolder   string        `env:"TEMP_FOLDER" envDefault:"${HOME}/tmp" envExpand:"true"`
+}
+
+func main() {
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	fmt.Printf("%+v\n", cfg)
+}
---
+++ misc/go.mod
@@ -0,0 +1,5 @@
+module misc
+
+go 1.19
+
+require github.com/caarlos0/env/v7 v7.0.0
