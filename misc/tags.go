819f43d0dc23b8d421ee863edbcb6996bb89ae3a
Xiatong Zheng <xiatong.zheng@deepl.com>
2023-01-31 17:37:53 +0100

    feat: tags

---
+++ misc/tags.go
@@ -0,0 +1,28 @@
+package misc
+
+//package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	type Me struct {
+		Firstname string `mytag:"FirstName"`
+		Lastname  string `mytag:"LastName"`
+	}
+
+	m := Me{"Anjana", "Shankar"}
+	t := reflect.TypeOf(m)
+
+	for _, fieldName := range []string{"Firstname", "Lastname"} {
+		field, found := t.FieldByName(fieldName)
+		if !found {
+			continue
+		}
+		fmt.Printf("\nField: Me.%s\n", fieldName)
+		fmt.Printf("\tWhole tag value : %q\n", field.Tag)
+		fmt.Printf("\tValue of 'mytag': %q\n", field.Tag.Get("mytag"))
+	}
+}
